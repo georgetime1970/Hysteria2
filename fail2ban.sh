dee966966b9dc02f7308234138c28e2d58943379
george <nihaokexiao@gmali.com>
2025-10-23 14:59:15 +0800

    完善fail2ban脚本

--- fail2ban.sh
+++ fail2ban.sh
@@ -12,43 +12,48 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
 echo
-echo -e "${RED}fail2ban 是用于保护服务器,防止 SSH 端口被恶意爆破的程序${NC}"
-echo -e "${RED}你将自定义设置失败几次后封禁恶意 IP 和封禁的时长${NC}"
+echo -e "${GREEN}fail2ban 是用于保护服务器,防止 SSH 端口被恶意爆破的程序${NC}"
+echo -e "${GREEN}你将自定义设置失败几次后封禁恶意 IP 和封禁的时长${NC}"
 echo "😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎😎"
 echo
-read -p "回车继续... Ctrl+c 退出" ENTER
 # 获取用户输入的失败次数和封禁时间
 read -p "请输入允许的最大失败登录次数（默认 5 次）: " MAXRETRY
 MAXRETRY=${MAXRETRY:-5}   
 read -p "请输入封禁时间，单位为秒（默认 864000 秒 10天）: " BAN_TIME
 BAN_TIME=${BAN_TIME:-864000}
 
-# 创建 /var/log/auth.log 文件（如果不存在）
-if [ ! -f /var/log/auth.log ]; then
-    touch /var/log/auth.log
-fi
 # 安装 fail2ban
 sudo apt update
 sudo apt install fail2ban -y
 
-# 创建本地配置文件
-sudo  cat > /etc/fail2ban/jail.local << EOF
-[sshd]
-enabled = true
-backend = auto
-logpath = /var/log/auth.log
-maxretry = $MAXRETRY
+# 写入配置文件
+sudo tee /etc/fail2ban/jail.local > /dev/null << EOF
+[DEFAULT]
 bantime = $BAN_TIME
 findtime = 600
+maxretry = $MAXRETRY
+ignoreip = 127.0.0.1/8 ::1
+
+[sshd]
+enabled = true
+backend = systemd
 EOF
 
+# 检查配置语法
+fail2ban-client -d >/dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "❌ 配置有误，请检查 /etc/fail2ban/jail.local"
+    exit 1
+fi
+
 # 启动并设置开机自启 fail2ban 服务
-sudo systemctl start fail2ban
-sudo systemctl enable fail2ban
+systemctl enable --now fail2ban
 
 # 查看Fail2ban状态
-sudo systemctl status fail2ban
+sudo systemctl status fail2ban | head -n 10
 
 # 显示爆破状态信息
-echo -e "${GREEN}fail2ban 已成功安装并配置完成！${NC}"
-sudo sudo fail2ban-client status sshd
+echo
+echo -e "${GREEN}✅ fail2ban 已成功安装并配置完成！${NC}"
+echo -e "${GREEN}使用命令 sudo fail2ban-client status sshd 即可查看以下封禁情况${NC}"
+sudo fail2ban-client status sshd
